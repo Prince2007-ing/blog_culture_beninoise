97f75fd197abeea04404e0062ccb02d12bb868c1
Prince Bignon <pdjomamou@gmail.com>
2025-09-09 17:53:38 +0100

    La base de donnée est déja créée et fonctionnelle. Voici le fichier de configuration pour le projet et un petit test.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+$host = "localhost";
+$user = "root" ;
+$password = 'openMySQLPrince2007@';
+$dbname = "blog_benin";
+
+// Create database connection
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
